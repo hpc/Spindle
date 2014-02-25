21e48d11e8cd1d71ff815397f0012f837b3fe950
Matthew LeGendre <legendre1@llnl.gov>
2014-02-25 12:36:30 -0800

    Initial work on PPC64 binding of symbols in GOT

--- src/client/auditclient/auditclient_ppc64.c
+++ src/client/auditclient/auditclient_ppc64.c
@@ -17,6 +17,37 @@ Place, Suite 330, Boston, MA 02111-1307 USA
 #include "client.h"
 #include "auditclient.h"
 
+static Elf64_Addr getTOCValue(struct link_map *map)
+{
+   //Get ELF Header
+   //Get TOC from e_entry
+
+   //Or is target a proper PPC64 indirect function pointer?
+}
+
+static Elf64_Addr doPermanentBinding(struct link_map *rmap,
+                                     struct link_map *dmap,
+                                     unsigned long plt_reloc_idx,
+                                     Elf64_Addr target)
+{
+   Elf64_Dyn *dynamic_section = rmap->l_ld;
+   Elf64_Rela *rel = NULL;
+   Elf64_Addr *got_entry;
+   Elf64_Addr base = rmap->l_addr;
+   for (; dynamic_section->d_tag != DT_NULL; dynamic_section++) {
+      if (dynamic_section->d_tag == DT_JMPREL) {
+         rel = ((Elf64_Rela *) dynamic_section->d_un.d_ptr) + plt_reloc_idx;
+         break;
+      }
+   }
+   if (!rel)
+      return target;
+   got_entry = (Elf64_Addr *) (rel->r_offset + base);
+   got_entry[0] = target;
+   got_entry[1] = ;
+   return target;
+}
+
 Elf64_Addr la_ppc64_gnu_pltenter(Elf64_Sym *sym,
                                  unsigned int ndx,
                                  uintptr_t *refcook,
@@ -26,8 +57,9 @@ Elf64_Addr la_ppc64_gnu_pltenter(Elf64_Sym *sym,
                                  const char *symname,
                                  long int *framesizep)
 {
-   struct link_map *map = get_linkmap_from_cookie(refcook);
+   struct link_map *rmap = get_linkmap_from_cookie(refcook);
+   struct link_map *dmap = get_linkmap_from_cookie(defcook);
+   unsigned long reloc_index = regs->lr_reg[0];
    Elf64_Addr target = client_call_binding(symname, sym->st_value);
-   /* return doPermanentBinding();  */
-   return target;
+   return doPermanentBinding(rmap, dmap, reloc_index, target);
 }
