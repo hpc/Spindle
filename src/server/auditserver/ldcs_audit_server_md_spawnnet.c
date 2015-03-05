2daa7bd60659012dcc90460b585f122a6c72787b
Matthew LeGendre <legendre1@llnl.gov>
2015-03-05 13:20:41 -0800

    Use thread to map from spawnnet to select

--- src/server/auditserver/ldcs_audit_server_md_spawnnet.c
+++ src/server/auditserver/ldcs_audit_server_md_spawnnet.c
@@ -15,10 +15,83 @@
 */
 
 #include "ldcs_audit_server_md.h"
+#include "spindle_debug.h"
+
+#include <unistd.h>
+#include <pthread.h>
+#include <errno.h>
+#include <stdlib.h>
+
+static int waitfor_spawnnet();
+
+static int read_pipe;
+static int write_pipe;
+static pthread_t thrd;
+
+static void *select_thread(void *arg)
+{
+   int result = 0;
+   char c = 'x';
+   ssize_t write_result;
+   for (;;) {
+      result = waitfor_spawnnet();
+      if (result == -1)
+         return;
+
+      write_result = write(write_pipe, &c, sizeof(c));
+      if (write_result == -1) {
+         err_printf("Failed to write to write pipe: %s", strerror(errno));
+         return NULL;
+      }
+   }
+   return NULL;
+}
+
+static void clear_pipe()
+{
+   char c;
+   ssize_t result = read(read_pipe, &c, sizeof(c));
+   if (result == -1) {
+      err_printf("Failed to read from read pipe: %s", strerror(errno));
+   }
+}
+
+static int waitfor_spawnnet()
+{
+   //TODO: Block on spawnnet's 'select' equivalent.  Return 0
+   // when data available or any change.  Return -1 when done.
+   // This is called on a thread.
+   return 0;
+}
+
+static int on_data(int fd, int id, void *data)
+{
+   ldcs_process_data_t *ldcs_process_data = ( ldcs_process_data_t *) data;
+   double starttime;
+   int result;
+
+   clear_pipe();
+   starttime = ldcs_get_time();
+
+   ldcs_message_t msg;
+   node_peer_t peer;
+   //TODO: Read a message into msg.  Peer is who sent the packet
+
+   result = handle_server_message(ldcs_process_data, peer, &msg);
+
+   ldcs_process_data->server_stat.md_cb.cnt++;
+   ldcs_process_data->server_stat.md_cb.time+=(ldcs_get_time() - starttime);
+
+   if (msg.data)
+      free(msg.data);
+   
+   return 0;
+}
 
 int initialize_handshake_security(void *protocol)
 {
-   return -1;
+#warning implement security
+   return 0;
 }
 
 int ldcs_audit_server_md_init(unsigned int port, unsigned int num_ports, unique_id_t unique_id, ldcs_process_data_t *data)
@@ -28,7 +101,26 @@ int ldcs_audit_server_md_init(unsigned int port, unsigned int num_ports, unique_
 
 int ldcs_audit_server_md_register_fd ( ldcs_process_data_t *data )
 {
-  return -1;
+   int result;
+   int pipefd[2];
+
+   result = pipe(pipefd);
+   if (result == -1) {
+      err_printf("Failed to create pipe: %s\n", strerror(errno));
+      return -1;
+   }
+   read_pipe = pipefd[0];
+   write_pipe = pipefd[1];
+   
+   result = pthread_create(&thrd, NULL, select_thread, NULL);
+   if (result == -1) {
+      err_printf("Failed to spawn thread: %s\n", strerror(errno));
+      return -1;
+   }
+
+   ldcs_listen_register_fd(read_pipe, read_pipe, on_data, data);
+      
+   return 0;
 }
 
 int ldcs_audit_server_md_unregister_fd ( ldcs_process_data_t *data )
