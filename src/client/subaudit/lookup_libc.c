16c9e2f9aa286fd7edd62ce9e03518102a0146e9
Matthew LeGendre <legendre1@llnl.gov>
2017-04-07 16:51:38 -0700

    Fix elf_hash bug that would have broken custom-built glibc's on BG/Q

--- src/client/subaudit/lookup_libc.c
+++ src/client/subaudit/lookup_libc.c
@@ -74,7 +74,7 @@ static unsigned long elf_hash(const unsigned char *name)
    while (*name) {
       h = (h << 4) + *name++;
       if ((g = h & 0xf0000000))
-         h ^= h >> 24;
+         h ^= g >> 24;
       h &= ~g;
    }
    return h;
