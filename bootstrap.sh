27bf26ce5ba3a71bcc693155038a55f7440a5871
Matthew LeGendre <mattpleg@gmail.com>
2024-10-24 14:29:42 -0700

    Turn off automake maintainer mode. Devs will now need to manually run bootstrap.sh after changing build system, but this fixes user issues with unnecessary reconfs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,35 +1,39 @@
 #!/bin/sh
 
 echo Bootstrap in spindle
+rm -rf autom4te.cache/
+aclocal
 autoheader
 autoconf
-aclocal
 automake --add-missing
 automake
 
 echo Bootstrap in client
 cd src/client
+rm -rf autom4te.cache/
+aclocal
 autoheader
 autoconf
-aclocal
 automake --add-missing
 automake
 cd ../..
 
 echo Bootstrap in server
 cd src/server
+rm -rf autom4te.cache/
+aclocal
 autoheader
 autoconf
-aclocal
 automake --add-missing
 automake
 cd ../..
 
 echo Bootstrap in fe
 cd src/fe
+rm -rf autom4te.cache/
+aclocal
 autoheader
 autoconf
-aclocal
 automake --add-missing
 automake
 cd ../..
