27fe4de717fcd2b2aa9ef3c192d7f26d6e50a317
Barry <rountree4@llnl.gov>
2025-08-22 08:05:53 -0700

    Moves filemngt initialization to message handler.
    ldcs_audit_server_filemngt_init() now called in
    ldcs_audit_server_handlers.c

--- src/server/auditserver/ldcs_audit_server_handlers.c
+++ src/server/auditserver/ldcs_audit_server_handlers.c
@@ -2947,6 +2947,10 @@ static int handle_client_pickone_msg(ldcs_process_data_t *procdata, int nc, ldcs
 static int handle_location_consensus(ldcs_process_data_t *procdata, ldcs_message_t *msg){
     int num_children = ldcs_audit_server_md_get_num_children(procdata);
 
+    debug_printf3("Initializing file cache location %s\n", procdata->location);
+    ldcs_audit_server_filemngt_init(procdata->location, procdata->commpaths, procdata->cachepaths);
+
+
     // FIXME:  Modify either the process data or the message with a bit index of working
     // directories.
     if (num_children) {
