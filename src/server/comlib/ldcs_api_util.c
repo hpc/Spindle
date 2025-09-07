f6f4dc705e0a9dc08c319a305dbb178f76ebb53b
Barry <rountree4@llnl.gov>
2025-09-07 11:40:16 -0700

    Adds messages LDCS_MSG_CHOSEN_CACHEPATH[_REQUEST]

--- src/server/comlib/ldcs_api_util.c
+++ src/server/comlib/ldcs_api_util.c
@@ -92,6 +92,8 @@ char* _message_type_to_str (ldcs_message_ids_t type) {
       STR_CASE(LDCS_MSG_ALIVE_REQ);
       STR_CASE(LDCS_MSG_ALIVE_RESP);      
       STR_CASE(LDCS_MSG_REQUEST_CACHEPATH_CONSENSUS);
+      STR_CASE(LDCS_MSG_CHOSEN_CACHEPATH_REQUEST);
+      STR_CASE(LDCS_MSG_CHOSEN_CACHEPATH);
       STR_CASE(LDCS_MSG_UNKNOWN);
    }
    return "unknown";
