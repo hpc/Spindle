d0cb0763db9538e918ab63dc5a3210a5ad3563f2
Barry <rountree4@llnl.gov>
2025-09-12 05:44:35 -0700

    Fixes inadvertently dropped line.

--- src/server/startup/spindle_be.cc
+++ src/server/startup/spindle_be.cc
@@ -154,6 +154,8 @@ int spindleRunBE(unsigned int port, unsigned int num_ports, unique_id_t unique_i
    free(args.location);
    args.location = new_location;
 
+   determineValidCachePaths( &args.cachepath_bitidx, args.candidate_cachepaths, args.number);
+
    result = ldcs_audit_server_process(&args);
    if (result == -1) {
       err_printf("Error in ldcs_audit_server_process\n");
