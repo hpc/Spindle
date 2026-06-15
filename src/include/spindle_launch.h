dafe83b2c986f93588449d36561b90fb6b4b4b64
Nicholas Chaimov <nchaimov@paratools.com>
2026-06-15 14:58:40 -0700

    Crash handler: --crash-dedup option
    Adds --crash-dedup option to control whether crash deduplication is
    enabled.

--- src/include/spindle_launch.h
+++ src/include/spindle_launch.h
@@ -60,7 +60,8 @@ extern "C" {
 #define OPT_NUMA       (1 << 29)            /* Enables file replication across NUMA domains */
 #define OPT_OFF        (1 << 30)            /* Turns spindle off, disabling everything */
 #define OPT_PATCHLDSO  (1 << 31)            /* Enables patching of ld.so to intercept stat calls */
-   
+#define OPT_CRASH_HANDLER ((opt_t) 1 << 32) /* Enables crash handler with coredump deduplication */
+
 #define OPT_SET_SEC(OPT, X) OPT |= (X << 19)
 #define OPT_GET_SEC(OPT) ((OPT >> 19) & 7)
 #define OPT_SEC_MUNGE 0                     /* Use munge to validate connections */
